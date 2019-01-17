01a22e4a789255db0665edb0a8364c0e8d3fbeda
Varga Balint <vargabalint@gmail.com>
2019-01-17 12:50:22 +0100

    gitignore & first java codes

---
+++ week-01/day04/practice/src/helloClass.java
@@ -0,0 +1,7 @@
+public class helloClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, Zoli");
+        System.out.println("Hello, Gabi");
+        System.out.println("Hello, Szabi");
+    }
+}
