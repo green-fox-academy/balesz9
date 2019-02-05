7d38d862dcc80db8b615221c5ac0f09d03e07bd4
Varga Balint <vargabalint@gmail.com>
2019-02-05 16:40:45 +0100

    Greenfox inheritance

---
+++ week-04/day02/homework/src/GreenFox/Person.java
@@ -0,0 +1,18 @@
+package GreenFox;
+
+public class Person {
+        String name;
+        int age;
+        String gender;
+
+        public String introduce(String name, int age, String gender){
+                return "Hi, I'm"+ this.name+ ", a "+ this.age+" year old "+this.gender+".";
+        }
+        public String getGoal(){
+                return "My goal is: Live for the moment!";
+        }
+
+        public
+}
+
+
