4ceba13c3098cddab57e9c801dc0cd4c570a73cd
Varga Balint <vargabalint@gmail.com>
2019-02-26 16:22:15 +0100

    add counter

---
+++ week-06/day02/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTControllerFromURL.java
@@ -0,0 +1,25 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import com.greenfoxacademy.springstart.Greeting;
+import com.greenfoxacademy.springstart.SpringstartApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class HelloRESTControllerFromURL {
+
+
+    static AtomicLong id = new AtomicLong(0);
+
+    @GetMapping(value="/greeting")
+    public static Greeting greeting(@RequestParam String name){
+        return new Greeting(id.getAndIncrement(),"Hello," + name + " !");
+    }
+}
+
