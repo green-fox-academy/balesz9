be8aff2e71656c0b1bc42ac6f765e6288fe5150c
Varga Balint <vargabalint@gmail.com>
2019-03-08 16:46:55 +0100

    add 'add more books link'

--- week-07/day02/library/src/main/java/com/greenfoxacademy/library/BookController.java
+++ week-07/day02/library/src/main/java/com/greenfoxacademy/library/BookController.java
@@ -2,12 +2,11 @@ package com.greenfoxacademy.library;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class BookController {
@@ -19,11 +18,11 @@ public class BookController {
         books.add(new Book("Do Androids Dream of Electric Sheep?", "Philip K. Dick", 1968));
     }
 
-    @RequestMapping(path = "/books", method = RequestMethod.GET)
+   /* @RequestMapping(path = "/books", method = RequestMethod.GET)
     public String showBooks(Model model) {
         model.addAttribute("books", books);
         return "index";
-    }
+    }*/
 
     @RequestMapping(path = "/books/{id}/details", method = RequestMethod.GET)
     public String getBookById(Model model, @PathVariable(name="id") Integer id) {
@@ -42,6 +41,39 @@ public class BookController {
         }
 
         return "details";
+    }
+
+    @RequestMapping(path = "/books", method = RequestMethod.GET)
+    public String queryBooks(Model model, @RequestParam(name = "author", required = false) String author) {
+
+        List<Book> queriedBooks;
+
+        if (author != null) {
+            queriedBooks = filterBooksByAuthor(author);
+        } else {
+            queriedBooks = books;
+        }
+
+        model.addAttribute("books", queriedBooks);
+        return "index";
+    }
+
+    private List<Book> filterBooksByAuthor(String author) {
+        return books.stream()
+                .filter(book -> book.getAuthor().equals(author))
+                .collect(Collectors.toList());
+    }
+
+    @RequestMapping(path = "/books/add", method = RequestMethod.GET)
+    public String addBookForm(Model model, @ModelAttribute(name="book") Book book) {
+        model.addAttribute("book", book);
+        return "create";
+    }
+
+    @RequestMapping(path = "/books/add", method = RequestMethod.POST)
+    public String addBook(@ModelAttribute(name="book") Book book) {
+        books.add(book);
+        return "redirect:/books";
     }
 
 }
