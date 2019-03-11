6a74add55a6718120d31bb5c08dd7b68abf1ebf1
Varga Balint <vargabalint@gmail.com>
2019-03-11 11:33:03 +0100

    add Todo class

---
+++ week-08/day01/src/main/java/com/greenfox/listingtodos/model/Todo.java
@@ -0,0 +1,9 @@
+package com.greenfox.listingtodos.model;
+
+public class Todo {
+
+    private long id;
+    private String title;
+    private boolean urgent = false;
+    private boolean done = false;
+}
