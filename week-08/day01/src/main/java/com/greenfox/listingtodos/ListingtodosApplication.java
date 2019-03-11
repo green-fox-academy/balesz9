51f0801a2ced46e4d5214d2aad1a4896e40ec101
Varga Balint <vargabalint@gmail.com>
2019-03-11 13:05:40 +0100

    populate database in the Application class

--- week-08/day01/src/main/java/com/greenfox/listingtodos/ListingtodosApplication.java
+++ week-08/day01/src/main/java/com/greenfox/listingtodos/ListingtodosApplication.java
@@ -1,13 +1,28 @@
 package com.greenfox.listingtodos;
 
+import com.greenfox.listingtodos.model.Todo;
+import com.greenfox.listingtodos.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ListingtodosApplication {
+public class ListingtodosApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ListingtodosApplication.class, args);
+    TodoRepository todoRepository;
+
+    @Autowired
+    public ListingtodosApplication(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
     }
 
+    public static void main(String[] args) {
+		SpringApplication.run(ListingtodosApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        todoRepository.save(new Todo("I have to learn Object Relational Mapping", false, false));
+    }
 }
--- week-08/day01/src/main/java/com/greenfox/listingtodos/controller/TodoController.java
+++ week-08/day01/src/main/java/com/greenfox/listingtodos/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.greenfox.listingtodos.controller;
 import com.greenfox.listingtodos.repository.TodoRepository;
 import com.greenfox.listingtodos.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
