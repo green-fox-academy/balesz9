f7fd204686b02edb2cefba76906dae9d23136c11
Varga Balint <vargabalint@gmail.com>
2019-03-16 22:56:26 +0100

    add static index to Fox LCub

---
+++ week-07/day05/ProgrammerFoxClub/src/main/java/com/greenfoxacadmy/programmerfoxclub/ProgrammerfoxclubApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacadmy.programmerfoxclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammerfoxclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammerfoxclubApplication.class, args);
+    }
+
+}
