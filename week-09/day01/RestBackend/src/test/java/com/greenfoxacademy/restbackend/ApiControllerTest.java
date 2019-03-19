398f1367097d96073499930b99846ca35a7bcba4
Varga Balint <vargabalint@gmail.com>
2019-03-19 17:06:38 +0100

    create test for append and dountil(sum)

--- week-09/day01/RestBackend/src/test/java/com/greenfoxacademy/restbackend/ApiControllerTest.java
+++ week-09/day01/RestBackend/src/test/java/com/greenfoxacademy/restbackend/ApiControllerTest.java
@@ -3,16 +3,24 @@ package com.greenfoxacademy.restbackend;
 
 import com.greenfoxacademy.restbackend.controller.ApiController;
 import com.greenfoxacademy.restbackend.service.MyService;
+import javafx.animation.Animation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ApiController.class)
@@ -44,4 +52,46 @@ public class ApiControllerTest {
                         .value("Oh, hi there Béla, my dear király!"));
 
     }
+
+    @Test
+    public void greeter_ReturnError_WithoutNameAndTitle() throws Exception {
+
+        mockMvc.perform(
+                get("/greeter?"))
+                .andExpect(MockMvcResultMatchers.jsonPath("error")
+                        .value("Please provide a name and a title!"));
+
+    }
+
+    @Test
+    public void append_ReturnWordWithAAppendad() throws Exception {
+
+        mockMvc.perform(
+                get("/appenda/macsk"))
+                .andExpect(MockMvcResultMatchers.jsonPath("appended")
+                        .value("macska"))
+                .andExpect(status().isOk());;
+    }
+
+    @Test
+    public void append_WithoutInput() throws Exception {
+
+        mockMvc.perform(
+                get("/appenda/"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void doUntil_ReturnSum_IsOk() throws Exception{
+
+
+        mockMvc.perform(post("/dountil/sum")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"until\": 5}"))
+                .andExpect(jsonPath("$.result", is(15)))
+                .andExpect(status().isOk());
+    }
+
+
 }
+
